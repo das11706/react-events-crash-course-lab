3e2562e01b0de58bc13edb2f53c7250457bfe8e0
Daniel Suarez <suarez.daniel101@gmail.com>
2023-06-01 17:57:18 -0400

    commit tree fcc9aaae13db6a3f1fad69253fddd750761e8429

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -32,10 +32,10 @@ export default class ChromeBoisDomain extends Component {
   };
 
   handleKeyPress = (event) => {
-    if (event.key == "a") {
+    if (event.key === 'a') {
     resize('+');
     }
-    else if (event.key == "s") {
+    else if (event.key === 's') {
     resize('-');
     }
   };
